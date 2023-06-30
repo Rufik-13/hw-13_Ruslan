e514093a105a7fab78d284375e6fddf99b64ae89
Ruf <rmedetbayev@gmail.com>
2023-06-30 13:52:29 +0600

    Initial Commit

---
+++ hw-13_Ruslan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw-13_Ruslan
+//
+//  Created by Ruslan Medetbayev on 30.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
